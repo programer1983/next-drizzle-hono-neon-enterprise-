a3c20f14ba53c7a3374e459f75cb20563b48903c
Prodi <programer972@gmail.com>
2026-05-07 12:08:33 +0300

    Installing Next.js, installing Hono, and checking Next.js requests to Hono and server data responses

---
+++ app/api/[[...route]]/route.ts
@@ -0,0 +1,11 @@
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+
+const app = new Hono().basePath("/api");
+
+app.get("/hello", (c) => {
+  return c.json({ message: "Hello from Hono!" });
+});
+
+export const GET = handle(app);
+export const POST = handle(app);
