a7e31d2616988892caa283b1196236d157ba14f7
Prodi <programer972@gmail.com>
2026-05-08 14:27:08 +0300

    connecting the main endpoints to the main hono file

--- app/api/[[...route]]/controllers/user.controllers.ts
+++ app/api/[[...route]]/controllers/user.controllers.ts
@@ -2,7 +2,11 @@ import { Context } from "hono";
 
 export const getUser = async (c: Context) => {
   return c.json({
-    users: [{ id: 1, name: "Jon Dorian" }],
+    users: [
+      { id: 1, name: "Jon Dorian" },
+      { id: 2, name: "Eliot Rid" },
+      { id: 3, name: "Cristofer Tyorcalton" },
+    ],
     message: "List of users received",
   });
 };
--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -6,6 +6,18 @@ const app = new Hono().basePath("/api");
 
 app.route("/user", userRouting);
 
+app.get("/", (c) => {
+  return c.json({
+    message:
+      "Welcome to Productify API - Powered by PostgreSQL, Drizzle ORM & Clerk Auth",
+    endpoints: {
+      users: "/api/users",
+      products: "/api/products",
+      comments: "/api/comments",
+    },
+  });
+});
+
 export const GET = handle(app);
 export const POST = handle(app);
 export const PUT = handle(app);
