951a6299c6ac0d6944fdd0afc2acaf8b31217b86
Prodi <programer972@gmail.com>
2026-05-17 12:37:43 +0300

    all routes for products

--- app/api/[[...route]]/routes/product.routes.ts
+++ app/api/[[...route]]/routes/product.routes.ts
@@ -4,5 +4,10 @@ import * as productController from "../controllers/product.comtroller";
 const router = new Hono();
 
 router.get("/", productController.getAllProducts);
+router.get("/my", productController.getMyProducts);
+router.get("/:id", productController.getProductById);
+router.post("/", productController.createProduct);
+router.put("/:id", productController.updateProduct);
+router.delete("/:id", productController.deleteProduct);
 
 export default router;
