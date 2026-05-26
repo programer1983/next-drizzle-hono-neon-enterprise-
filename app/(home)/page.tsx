fac6b5cc84779ebc668a2258ee5ec2e6fc9ae927
Prodi <programer972@gmail.com>
2026-05-26 13:24:12 +0300

    Creating a custom hook for products, with the inclusion of tanstackquery - useQuery, useMutation, useQueryClient

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,8 +1,11 @@
+import HomePage from "@/components/HomePage";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-base-100">
-      <main className="max-w-5xl mx-auto px-4 py-8">home</main>
+      <main className="max-w-5xl mx-auto px-4 py-8">
+        <HomePage />
+      </main>
     </div>
   );
 }
