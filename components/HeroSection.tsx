62beec77be3c3d70e321544b321baf6170d10c6e
Prodi <programer972@gmail.com>
2026-06-11 11:45:41 +0300

    fixed button in hero section

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,8 +1,9 @@
+import { SparklesIcon, PlusCircleIcon } from "lucide-react";
+import { SignInButton, useAuth } from "@clerk/nextjs";
 import Link from "next/link";
-import { SparklesIcon } from "lucide-react";
-import { SignInButton } from "@clerk/nextjs";
 
 export default function HeroSection() {
+  const { isSignedIn } = useAuth();
   return (
     <div className="hero bg-linear-to-br from-base-300 via-base-200 to-base-300 rounded-boxn overflow-hidden">
       <div className="hero-content flex-col lg:flex-row-reverse gap-10 py-10">
@@ -23,12 +24,20 @@ export default function HeroSection() {
           <p className="py-4 text-base-content/60">
             Upload, discover, and connect with creators.
           </p>
-          <SignInButton mode="modal">
-            <button className="btn btn-primary">
-              <SparklesIcon className="size-4" />
-              Start Selling
-            </button>
-          </SignInButton>
+
+          {isSignedIn ? (
+            <Link href="/create" className="btn btn-primary">
+              <PlusCircleIcon className="size-4" />
+              Add Product
+            </Link>
+          ) : (
+            <SignInButton mode="modal">
+              <button className="btn btn-primary">
+                <SparklesIcon className="size-4" />
+                Start Selling
+              </button>
+            </SignInButton>
+          )}
         </div>
       </div>
     </div>
